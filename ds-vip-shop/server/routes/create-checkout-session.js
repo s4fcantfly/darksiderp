845020f74f3e42491f0656c1e8b67184e635936e
s4f <safwane.rifki@gmail.com>
2025-12-26 03:27:56 +0100

    Convert car images from PNG to WebP and update references in vip.html

--- ds-vip-shop/server/routes/create-checkout-session.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const checkoutController = require('../controllers/checkoutController');
-
-// Route to create a Stripe Checkout session
-router.post('/', checkoutController.createCheckoutSession);
-
-module.exports = router;
